9dfa8a7921a4569b69fc0019f22fc4f2687121b5
tomasVergara14 <tomasvergara272@gmail.com>
2020-11-11 16:42:42 -0300

    arregle la vista en el localhost

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.static('public'));
+app.use(express.static('views'));
 
+app.get('/', (req,res)=>{
+     
+    res.sendFile( __dirname + '/' + req.url )});
+
+
+app.get ('*', function (req, res) {
+        res.sendFile(__dirname + '/' + req.url)
+    })
 
 
 
 
 app.listen(3000, function (){
-    console.log("Corriendo en puerto 3000")
-});
+    "corriendo en puerto 3000";
+})
